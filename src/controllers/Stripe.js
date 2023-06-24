761cc45fa61f7724974c17982be908a2478c58d6
Matt Holt <jamesmattholt@gmail.com>
2023-06-24 12:10:58 +0100

    Shipping cost varies based on country collected by client

--- src/controllers/Stripe.js
+++ src/controllers/Stripe.js
@@ -3,9 +3,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY, {
 });
 
 const createCheckout = async (req, res) => {
-  const { newBasket } = req.body;
-  console.log(newBasket);
-
+  const { newBasket, shippingCost } = req.body;
   const line_items = newBasket.map((item) => {
     return {
       price_data: {
@@ -20,6 +18,31 @@ const createCheckout = async (req, res) => {
   });
 
   const session = await stripe.checkout.sessions.create({
+    // shipping_address_collection: {
+    //   allowed_countries: ['US', 'GB', 'FR'],
+    // },
+    shipping_options: [
+      {
+        shipping_rate_data: {
+          type: 'fixed_amount',
+          fixed_amount: {
+            amount: shippingCost * 100,
+            currency: 'gbp',
+          },
+          display_name: 'standard shipping',
+          delivery_estimate: {
+            minimum: {
+              unit: 'business_day',
+              value: 3,
+            },
+            maximum: {
+              unit: 'business_day',
+              value: 5,
+            },
+          },
+        },
+      },
+    ],
     line_items,
     mode: 'payment',
     success_url: 'http://localhost:3001/success',
